bf177a9afd5f59af61f5868a7132a088aa000b68
Ihar Trymailau <ihar.trymailau@gmail.com>
2020-02-19 22:56:56 +0300

    feat: add function (max, min, avg)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  return (array == undefined || array.length == 0) ? 0 : Math.min(...array);
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  return (array == undefined || array.length == 0) ? 0 : Math.max(...array);
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if(array == undefined || array.length == 0) return 0; 
+  
+  let sum = 0;
+  for(let i=0; i < array.length; i++) {
+      sum += array[i];
+  }
+  return sum / array.length;
+};
